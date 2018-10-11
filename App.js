eed5762679fd72b352ea408fcbf2dc65bd710a70
Chinmayi Prasad <chinmayi2398@gmail.com>
2018-10-11 17:43:33 +0530

    Update App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import DishPage from './components/DishPage';
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={{flex:1}}>
+      
+      </View>
+    );
+  }
+}
+
+
