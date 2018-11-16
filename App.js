f3d2251960e381f3f82350d21a7a914daee2f0de
Chinmayi Prasad <chinmayi2398@gmail.com>
2018-11-16 14:17:38 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,188 +1,194 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-/*
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+ import React, { Component } from 'react';
+ import {
+   StyleSheet,
+   Text,
+   View,
+   TouchableOpacity,
+   Image,
+   Alert,
+   ScrollView,
+   FlatList,
+ } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+ export default class Store extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+   constructor(props) {
+     super(props);
+     this.state = {
+      dish_list: [{dish_name: "IDLY",  price:"Rs. 25.00"},
+      {dish_name: "MASALA DOSA",  price:"Rs. 30.00"}
+      ],
+      images_lst : [
+        "http://2.bp.blogspot.com/-1Vy8ilCzfok/TacfNwOU_TI/AAAAAAAAAM8/50Y8NDG2FM8/s1600/Samabr.jpg",
+        "http://feenix.co.in/wp-content/uploads/2017/12/7-696x364.jpg",
+        "https://www.vegrecipesofindia.com/wp-content/uploads/2016/10/chow-chow-bhaat-recipe-2.jpg",
+        "https://www.vegrecipesofindia.com/wp-content/uploads/2016/09/methi-dosa-recipe-2.jpg",
+        "http://www.manjulaskitchen.com/blog/wp-content/uploads/aloo_paratha1.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/d/d8/Puri_A.jpg",
+       "https://lh4.ggpht.com/-br4XlnfcwRk/VK5yMKDlSuI/AAAAAAAAj9s/6dR0qKpfgL4/w1200-h630-p-k-no-nu/Chapati_thumb%25255B2%25255D.jpg?imgmax=800",
+       "https://thumbs.dreamstime.com/z/south-indian-meals-served-banana-leaf-traditional-cuisine-57371591.jpg",
+      ],
+       data: [
+         {title: "IDLY",  price:"Rs. 25.00", image:"http://2.bp.blogspot.com/-1Vy8ilCzfok/TacfNwOU_TI/AAAAAAAAAM8/50Y8NDG2FM8/s1600/Samabr.jpg"},
+         {title: "MASALA DOSA",  price:"Rs. 30.00", image:"http://feenix.co.in/wp-content/uploads/2017/12/7-696x364.jpg"} ,
+         {title: "CHOW CHOW BATH", price:"Rs 25.00", image:"https://www.vegrecipesofindia.com/wp-content/uploads/2016/10/chow-chow-bhaat-recipe-2.jpg"},
+         {title: "PLAIN DOSA",  price:"Rs. 25.00", image:"https://www.vegrecipesofindia.com/wp-content/uploads/2016/09/methi-dosa-recipe-2.jpg"},
+         {title: "ALOO PARATHA",  price:"Rs. 30.00", image:"http://www.manjulaskitchen.com/blog/wp-content/uploads/aloo_paratha1.jpg"},
+         {title: "PURI",  price:"Rs. 30.00", image:"https://upload.wikimedia.org/wikipedia/commons/d/d8/Puri_A.jpg"},
+         {title: "CHAPATI",  price:"Rs. 20.00", image:"https://lh4.ggpht.com/-br4XlnfcwRk/VK5yMKDlSuI/AAAAAAAAj9s/6dR0qKpfgL4/w1200-h630-p-k-no-nu/Chapati_thumb%25255B2%25255D.jpg?imgmax=800"},
+         {title: "PARATHA",  price:"Rs. 30.00", image:"https://www.google.co.in/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjfzOLy2cHeAhUSbisKHXXdDlEQjRx6BAgBEAU&url=https%3A%2F%2Ffood.ndtv.com%2Frecipe-kootoo-ka-parantha-98863&psig=AOvVaw0Cfq6_7Lorn5EyqAw97Wtp&ust=1541660161407241"},
+         {title: "SOUTH INDIAN MEALS",  price:"Rs. 60", image:"https://thumbs.dreamstime.com/z/south-indian-meals-served-banana-leaf-traditional-cuisine-57371591.jpg"},
+         {title: "NORTH INDIAN MEALS",  price:"Rs. 80", image:"https://www.google.co.in/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiavoe_18HeAhWSinAKHZVEAaUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.watscooking.com%2Fdishes%2Fnorth-indian-meals%2F839786&psig=AOvVaw1FZ4FG4gaOjfosGmi_VoK6&ust=1541659278718503"},
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-*/
+       ]
+     };
+   }
 
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, Dimensions,TouchableHighlight} from 'react-native';
-//import { SearchBar } from 'react-native-elements'
-  import { Button, SearchBar, Tile} from 'react-native-elements';
-  import Icon from 'react-native-vector-icons/FontAwesome';
-  //import { SearchBar } from 'react-native-elements'
+   addProductToCart = () => {
+     Alert.alert('Success', 'The product has been added to your cart')
+     var data = indian ;
+     alert(data);
+   }
 
-// function 
-onPress = () => {
-    console.log('vfid')
-  }
-something = () => {
-    console.log('print to console')
-    console.log(this.text)
-  }
+   render() {
+     return (
+       <View style={styles.container}>
+         <FlatList style={styles.list}
+           contentContainerStyle={styles.listContainer}
+           data={this.state.data}
+           horizontal={false}
+           numColumns={2}
+           keyExtractor= {(item) => {
+             return item.id;
+           }}
+           ItemSeparatorComponent={() => {
+             return (
+               <View style={styles.separator}/>
+             )
+           }}
+           renderItem={(post) => {
+             const item = post.item;
+             return (
+               <View style={styles.card}>
 
+                <View style={styles.cardHeader}>
+                   <View>
+                     <Text style={styles.title}>{item.title}</Text>
+                     <Text style={styles.price}>{item.price}</Text>
+                   </View>
+                 </View>
 
-export default class App extends Component {
-  constructor() {
-    super()
+                 <Image style={styles.cardImage} source={{uri:item.image}}/>
 
-    this.state = {
-      width: Dimensions.get('window').width
-    }
-  }
+                 <View style={styles.cardFooter}>
+                   <View style={styles.socialBarContainer}>
+                     <View style={styles.socialBarSection}>
+                       <TouchableOpacity style={styles.socialBarButton} onPress={() => this.addProductToCart()}>
+                         <Image style={styles.icon} source={{uri: 'https://png.icons8.com/nolan/96/3498db/add-shopping-cart.png'}}/>
+                         <Text style={[styles.socialBarLabel, styles.buyNow]}>Buy Now</Text>
+                       </TouchableOpacity>
+                     </View>
+                   </View>
+                 </View>
+               </View>
+             )
+           }}/>
+       </View>
+     );
+   }
+ }
 
-  render() {
-    const { width } = this.state;
-    return (
-
-  /*
-  <View>
-    <SearchBar
-  clearIcon={{ color: 'red' }}
-  searchIcon={false} // You could have passed `null` too
-  //onChangeText={someMethod}
-  //onClear={someMethod}
-  placeholder='Type Here...' />
-  <Button title="Click" />
-  
-  <Tile
-  imageSrc={require('./src/img/iron_man.jpg')}
-  title="Iron man"
-  featured
-  caption="Some Caption Text" />
-
-
-<Text>Hello World!</Text>  
-</View> */
-<View>
-<SearchBar
-  clearIcon={{ color: 'red' }}
-  searchIcon={false} // You could have passed `null` too
-  onChangeText={something(this.text)}
-  //onClear={someMethod}
-  placeholder='Type Here...' />
-  <Text> Choose your cuisine </Text>
-<View style={{
-        flex: 1,
-        flexWrap: 'wrap',
-        flexDirection: 'row',
-        backgroundColor: '#ecf0f1'
-      }}>
-        <Tile
-          width={width/2}
-          featured
-          caption="1"
-          //imageSrc={require('./src/img/iron_man.jpg')}
-          //title="Iron man"
-          imageSrc={require('./src/img/indian.jpg')}
-          title="Indian"
-
-        />
-        <Tile
-          width={width/2}
-          featured
-          caption="2"
-          imageSrc={require('./src/img/chinese.jpg')}
-          title="Chinese"
-
-        />
-        <Tile
-          width={width/2}
-          featured
-          caption="3"
-          imageSrc={require('./src/img/american.jpg')}
-          title="American"
-        />
-        <Tile
-          width={width/2}
-          featured  
-          caption="4"
-          imageSrc={require('./src/img/italian.jpg')}
-          title="Italian"
-        />
-        <Tile
-          width={width/2}
-          featured  
-          caption="5"
-          imageSrc={require('./src/img/kannada.jpg')}
-          title="Namma Nadu"
-        />
-
-        
-          <TouchableHighlight
-         style={styles.button}
-         onPress={this.onPress} >
-          <Tile
-          width={width/2}
-          featured  
-          caption="6"
-          imageSrc={require('./src/img/bakery.jpg')}
-          title="Bakery"
-          />
-         </TouchableHighlight>
-      </View>
-      </View>
-    );
-  }
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  half1: {
-    flex: 1,
-    //height: 'number, string'
-    backgroundColor: 'red'
-  },
-  half1: {
-    flex: 1,
-    //height: 'number, string'
-    backgroundColor: 'red'
-  }
-})
+ const styles = StyleSheet.create({
+   container:{
+     flex:1,
+     marginTop:20,
+   },
+   list: {
+     paddingHorizontal: 5,
+     backgroundColor:"#E6E6E6",
+   },
+   listContainer:{
+     alignItems:'center'
+   },
+   separator: {
+     marginTop: 10,
+   },
+   /******** card **************/
+   card:{
+     shadowColor: '#00000021',
+     shadowOffset: {
+       width: 2
+     },
+     shadowOpacity: 0.5,
+     shadowRadius: 4,
+     marginVertical: 8,
+     backgroundColor:"white",
+     flexBasis: '47%',
+     marginHorizontal: 5,
+   },
+   cardHeader: {
+     paddingVertical: 17,
+     paddingHorizontal: 16,
+     borderTopLeftRadius: 1,
+     borderTopRightRadius: 1,
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+   },
+   cardContent: {
+     paddingVertical: 12.5,
+     paddingHorizontal: 16,
+   },
+   cardFooter:{
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     paddingTop: 12.5,
+     paddingBottom: 25,
+     paddingHorizontal: 16,
+     borderBottomLeftRadius: 1,
+     borderBottomRightRadius: 1,
+   },
+   cardImage:{
+     flex: 1,
+     height: 150,
+     width: null,
+   },
+   /******** card components **************/
+   title:{
+     fontSize:18,
+     flex:1,
+   },
+   price:{
+     fontSize:16,
+     color: "green",
+     marginTop: 5
+   },
+   buyNow:{
+     color: "purple",
+   },
+   icon: {
+     width:25,
+     height:25,
+   },
+   /******** social bar ******************/
+   socialBarContainer: {
+     justifyContent: 'center',
+     alignItems: 'center',
+     flexDirection: 'row',
+     flex: 1
+   },
+   socialBarSection: {
+     justifyContent: 'center',
+     flexDirection: 'row',
+     flex: 1,
+   },
+   socialBarlabel: {
+     marginLeft: 8,
+     alignSelf: 'flex-end',
+     justifyContent: 'center',
+   },
+   socialBarButton:{
+     flexDirection: 'row',
+     justifyContent: 'center',
+     alignItems: 'center',
+   }
+ });
